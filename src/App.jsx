79ebf85fb7779d937aa2fcdd547bda7a10eb8bfe
Jassi Singh <singhjassi2624@gmail.com>
2025-07-07 08:54:36 +0545

    fix: useEfffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-const CustomComponent = ({ title }) => {
-  const[count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState(0);
   const [liked, setLiked] = useState(false);
 
   useEffect(() => {
-    console.log('like on '+title + ' is '+liked)
-  }, [liked]);
+    console.log("render");
+  },[ liked]);
 
-
-
-  return (
-    <div className='card' onClick={() => setCount(count + 1)}>
-      <h1>{title}</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setLiked(!liked)}>
-        {liked ? '❤️' : '🤍'}
-      </button>
-    </div>
-  );
-};
-const App = () => {
- 
   return (
     <>
       <h1>Hello World</h1>
-      <CustomComponent title="POST 1" />
-      <CustomComponent title="POST 2" />
+      <h1>{count}</h1>
+      <button
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          padding: "40px 60px",
+          borderRadius: "5px",
+          border: "none",
+          cursor: "pointer",
+        }}
+        onClick={() => setCount(count + 1)}
+      >
+        Click me
+      </button>
+      <h1>{liked ? "liked" : "not liked"}</h1>
+      <button
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          padding: "40px 60px",
+          borderRadius: "5px",
+          border: "none",
+          cursor: "pointer",
+        }} onClick={() => setLiked(liked?false:true)}>{liked ? "❤️" : "🤍"}</button>
     </>
   );
 };
